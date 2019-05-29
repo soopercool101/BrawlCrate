92df49383109a6ca98d4ba8338f2c4e2582a0b3b
soopercool101 <soopercoolstages@gmail.com>
2019-05-29 12:36:01 -0700

    Dump textures script now dumps REFTEntries

--- BrawlCrate/Plugins/Dump Textures.py
+++ BrawlCrate/Plugins/Dump Textures.py
@@ -1,9 +1,9 @@
 from BrawlCrate.API import BrawlAPI
 from BrawlLib.SSBB.ResourceNodes import *
 
-def tex_search(node): #Recursive function to scan for all TEX0 nodes in the file
-    if isinstance(node, TEX0Node):
-        return [node] #If it's a TEX0 node, include it in the list
+def tex_search(node): #Recursive function to scan for all TEX0/REFTEntry nodes in the file
+    if isinstance(node, TEX0Node) or isinstance(node, REFTEntryNode):
+        return [node] #If it's a TEX0/REFTEntry node, include it in the list
     list = []
     for child in node.Children:
         list += tex_search(child) #Otherwise, keep looking for children
@@ -14,7 +14,7 @@ if BrawlAPI.RootNode != None:
     folder = BrawlAPI.OpenFolderDialog()
     if folder:
         for item in tex_search(root):
-            item.Export(folder + "/" + item.Name + ".png") #We already know everything in this list is a TEX0, so we can export it to a #PNG
+            item.Export(folder + "/" + item.Name + ".png") #We already know everything in this list is a TEX0 or REFTEntry, so we can export it to a .png
         BrawlAPI.ShowMessage("Textures successfully exported to " + folder, "Success")
 else:
     BrawlAPI.ShowMessage('Cannot find Root Node (is a file open?)','Error')
