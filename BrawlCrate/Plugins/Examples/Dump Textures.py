b0c961fb3ae2f0ae3a744dfeb59e6d176c0054b1
soopercool101 <soopercoolstages@gmail.com>
2019-06-20 17:56:20 -0700

    Improve dump textures commenting

--- BrawlCrate/Plugins/Examples/Dump Textures.py
+++ BrawlCrate/Plugins/Examples/Dump Textures.py
@@ -2,12 +2,12 @@ from BrawlCrate.API import BrawlAPI
 from BrawlLib.SSBB.ResourceNodes import *
 from BrawlLib.Imaging import IImageSource
 
-def tex_search(node): #Recursive function to scan for all image nodes in the file
+def tex_search(node): # Recursive function to scan for all image nodes in the file
     if isinstance(node, IImageSource):
-        return [node] #If it's an image node, include it in the list
+        return [node] # If it's an image node, include it in the list
     list = []
     for child in node.Children:
-        list += tex_search(child) #Otherwise, keep looking for children
+        list += tex_search(child) # Otherwise, keep looking for children
     return list
 
 if BrawlAPI.RootNode != None:
@@ -16,7 +16,7 @@ if BrawlAPI.RootNode != None:
     if folder:
         count = 0;
         for item in tex_search(root):
-            item.Export(folder + "/" + item.Name + ".png") #We already know everything in this list should be exportable to a .png
+            item.Export(folder + "/" + item.Name + ".png") # We already know everything in this list should be exportable to a .png
             count += 1
         if count:
             BrawlAPI.ShowMessage(str(count) + " textures were successfully exported to " + folder, "Success")
