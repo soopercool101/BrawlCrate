cd3aebfc428b7f32ce58cc924ca27e5644359504
soopercool101 <soopercoolstages@gmail.com>
2019-07-01 15:04:18 -0700

    Use more python-centric code rather than lazy C# imports

--- BrawlCrate/Loaders/Result Portraits Exporter.py
+++ BrawlCrate/Loaders/Result Portraits Exporter.py
@@ -2,7 +2,6 @@ from BrawlCrate.NodeWrappers import ARCWrapper
 from BrawlCrate.API import *
 from BrawlLib.SSBB.ResourceNodes import *
 from System.Windows.Forms import ToolStripMenuItem
-from System import String
 
 def export_to_results(sender, event_args):
     folder = BrawlAPI.OpenFolderDialog()
@@ -18,7 +17,7 @@ def export_to_results(sender, event_args):
             BrawlAPI.ShowMessage('No BRRESs were found in the open file','Error')
 
 def EnableCheck(sender, event_args):
-    sender.Enabled = BrawlAPI.SelectedNode.Name.Equals("char_bust_tex_lz77")
+    sender.Enabled = (BrawlAPI.SelectedNode.Name == "char_bust_tex_lz77")
 
 # Add a button to our right click menu
 BrawlAPI.AddContextMenuItem(ARCWrapper, "", "Exports the CSPs in this ARC to Results Screen formatted BRRESs", EnableCheck, ToolStripMenuItem("Export as RSPs", None, export_to_results))
