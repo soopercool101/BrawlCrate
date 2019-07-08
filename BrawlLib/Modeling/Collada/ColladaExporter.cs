68487e2f37214946e5670aa00c6781804c7d1e71
soopercool101 <soopercoolstages@gmail.com>
2019-07-08 13:43:03 -0700

    Fix bug in which exporting a model with a null texture will crash

--- BrawlLib/Modeling/Collada/ColladaExporter.cs
+++ BrawlLib/Modeling/Collada/ColladaExporter.cs
@@ -188,6 +188,10 @@ namespace BrawlLib.Modeling
                         {
                             foreach (MDL0MaterialRefNode mr in mat.Children)
                             {
+                                if (string.IsNullOrEmpty(mr._texture?.Name))
+                                {
+                                    continue;
+                                }
                                 writer.WriteStartElement("newparam");
                                 writer.WriteAttributeString("sid", mr._texture.Name + "-surface");
                                 {
