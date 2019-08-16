1e5a4a6b03361ff7ad1f14d0109ae91265045cce
soopercool101 <soopercoolstages@gmail.com>
2019-08-16 09:45:13 -0700

    Final "quote" test before I give up and revert to apostrophes

--- AppveyorDiscordHook.ps1
+++ AppveyorDiscordHook.ps1
@@ -37,8 +37,8 @@ if (!$env:APPVEYOR_REPO_COMMIT) {
 
 $AUTHOR_NAME="$(git log -1 "$env:APPVEYOR_REPO_COMMIT" --pretty="%aN")"
 $COMMITTER_NAME="$(git log -1 "$env:APPVEYOR_REPO_COMMIT" --pretty="%cN")"
-$COMMIT_SUBJECT="$(git log -1 "$env:APPVEYOR_REPO_COMMIT" --pretty="%s")" -replace "`"", "''"
-$COMMIT_MESSAGE="$(git log -1 "$env:APPVEYOR_REPO_COMMIT" --pretty="%b")" -replace "`"", "''"
+$COMMIT_SUBJECT="$(git log -1 "$env:APPVEYOR_REPO_COMMIT" --pretty="%s")" -replace "`"", "`"`""
+$COMMIT_MESSAGE="$(git log -1 "$env:APPVEYOR_REPO_COMMIT" --pretty="%b")" -replace "`"", "`"`""
 
 if ($AUTHOR_NAME -eq $COMMITTER_NAME) {
   $CREDITS="$AUTHOR_NAME authored & committed"
