bc89a9fed6b6b3542c8690d8d8b7a4912e66b747
soopercool101 <soopercoolstages@gmail.com>
2019-09-09 15:35:23 -0700

    Improve hotfix title shortening

--- BrawlCrate/Program.cs
+++ BrawlCrate/Program.cs
@@ -86,9 +86,7 @@ Full changelog can be viewed from the help menu.";
                 AssemblyTitleFull = AssemblyTitleFull.Replace("BrawlCrate", "PartyBrawl");
             }
 
-            AssemblyTitleShort = AssemblyTitleFull.Contains("Hotfix")
-                ? AssemblyTitleFull.Substring(0, AssemblyTitleFull.IndexOf("Hotfix")).Trim(' ')
-                : AssemblyTitleFull;
+            AssemblyTitleShort = AssemblyTitleFull.Replace(" Hotfix ", "h");
 #endif
             AssemblyDescription =
                 ((AssemblyDescriptionAttribute) Assembly
