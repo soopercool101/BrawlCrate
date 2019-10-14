dd50b5d5f208441b9af6ec9edd1b84b37836ce77
soopercool101 <soopercoolstages@gmail.com>
2019-10-14 16:21:45 -0700

    Add setting for loader disabling

--- BrawlCrate/Properties/Settings.Designer.cs
+++ BrawlCrate/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace BrawlCrate.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -305,6 +305,17 @@ namespace BrawlCrate.Properties {
             }
             set {
                 this["BuildPath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        public global::System.Collections.Specialized.StringCollection DisabledLoaders {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["DisabledLoaders"]));
+            }
+            set {
+                this["DisabledLoaders"] = value;
             }
         }
     }
