8a0355e982345063d8f58a46d81e904659dbcab3
soopercool101 <soopercoolstages@gmail.com>
2019-11-03 17:34:23 -0800

    Further improve error message

--- BrawlLib/SSBB/ResourceNodes/RSAR/File References/RSARSoundNode.cs
+++ BrawlLib/SSBB/ResourceNodes/RSAR/File References/RSARSoundNode.cs
@@ -744,7 +744,7 @@ namespace BrawlLib.SSBB.ResourceNodes
                 IAudioStream stream = CreateStreams()[0];
                 if (stream == null)
                 {
-                    throw new Exception($"{_soundFileNode.GetType().Name} cannot be exported to WAV.");
+                    throw new Exception($"{_soundFileNode?.GetType().Name ?? ""} \"{Name}\" cannot be exported to WAV.");
                 }
 
                 WAV.ToFile(stream, outPath);
