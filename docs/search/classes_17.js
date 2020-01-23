75d17dd57d6e2a190adfd16befc6862a68197e3a
soopercool101 <soopercoolstages@gmail.com>
2020-01-22 20:58:35 -0800

    Update documentation for v0.30g

--- docs/search/classes_17.js
+++ docs/search/classes_17.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['xfarrayflags_18574',['XFArrayFlags',['../struct_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_array_flags.html',1,'BrawlLib::Wii::Graphics']]],
-  ['xfcommand_18575',['XFCommand',['../struct_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_command.html',1,'BrawlLib::Wii::Graphics']]],
-  ['xfdata_18576',['XFData',['../class_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_data.html',1,'BrawlLib::Wii::Graphics']]],
-  ['xfdualtex_18577',['XFDualTex',['../struct_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_dual_tex.html',1,'BrawlLib::Wii::Graphics']]],
-  ['xftexmtxinfo_18578',['XFTexMtxInfo',['../struct_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_tex_mtx_info.html',1,'BrawlLib::Wii::Graphics']]],
-  ['xfvertexspecs_18579',['XFVertexSpecs',['../struct_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_vertex_specs.html',1,'BrawlLib::Wii::Graphics']]],
-  ['xmlreader_18580',['XmlReader',['../class_brawl_lib_1_1_internal_1_1_i_o_1_1_xml_reader.html',1,'BrawlLib::Internal::IO']]],
-  ['xytransform_18581',['xyTransform',['../class_brawl_crate_1_1_u_i_1_1xy_transform.html',1,'BrawlCrate::UI']]],
-  ['xyzpixel_18582',['XYZPixel',['../struct_brawl_lib_1_1_imaging_1_1_x_y_z_pixel.html',1,'BrawlLib::Imaging']]]
+  ['xfarrayflags_18580',['XFArrayFlags',['../struct_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_array_flags.html',1,'BrawlLib::Wii::Graphics']]],
+  ['xfcommand_18581',['XFCommand',['../struct_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_command.html',1,'BrawlLib::Wii::Graphics']]],
+  ['xfdata_18582',['XFData',['../class_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_data.html',1,'BrawlLib::Wii::Graphics']]],
+  ['xfdualtex_18583',['XFDualTex',['../struct_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_dual_tex.html',1,'BrawlLib::Wii::Graphics']]],
+  ['xftexmtxinfo_18584',['XFTexMtxInfo',['../struct_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_tex_mtx_info.html',1,'BrawlLib::Wii::Graphics']]],
+  ['xfvertexspecs_18585',['XFVertexSpecs',['../struct_brawl_lib_1_1_wii_1_1_graphics_1_1_x_f_vertex_specs.html',1,'BrawlLib::Wii::Graphics']]],
+  ['xmlreader_18586',['XmlReader',['../class_brawl_lib_1_1_internal_1_1_i_o_1_1_xml_reader.html',1,'BrawlLib::Internal::IO']]],
+  ['xytransform_18587',['xyTransform',['../class_brawl_crate_1_1_u_i_1_1xy_transform.html',1,'BrawlCrate::UI']]],
+  ['xyzpixel_18588',['XYZPixel',['../struct_brawl_lib_1_1_imaging_1_1_x_y_z_pixel.html',1,'BrawlLib::Imaging']]]
 ];
