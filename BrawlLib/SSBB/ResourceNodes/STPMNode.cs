a7f283689ba6a08c8102f9dc6c5baf4f78b3ac93
soopercool101 <soopercoolstages@gmail.com>
2020-03-09 15:10:24 -0700

    Add basic MVPM parsing

--- BrawlLib/SSBB/ResourceNodes/STPMNode.cs
+++ BrawlLib/SSBB/ResourceNodes/STPMNode.cs
@@ -207,7 +207,7 @@ namespace BrawlLib.SSBB.ResourceNodes
 
     public unsafe class STPMEntryNode : ResourceNode
     {
-        internal ParameterEntry* Header => (ParameterEntry*) WorkingUncompressed.Address;
+        internal StageParameterEntry* Header => (StageParameterEntry*) WorkingUncompressed.Address;
         public override ResourceType ResourceFileType => ResourceType.Unknown;
 
         private byte echo;
@@ -1004,8 +1004,8 @@ namespace BrawlLib.SSBB.ResourceNodes
 
         public override void OnRebuild(VoidPtr address, int length, bool force)
         {
-            ParameterEntry* header = (ParameterEntry*) address;
-            *header = new ParameterEntry(id, echo, id2);
+            StageParameterEntry* header = (StageParameterEntry*) address;
+            *header = new StageParameterEntry(id, echo, id2);
             byte* pOut = (byte*) header + 4;
             byte* pIn = (byte*) _values._values.Address;
             for (int i = 0; i < 64 * 4; i++)
