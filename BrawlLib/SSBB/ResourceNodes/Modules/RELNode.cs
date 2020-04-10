6f2f506535792bc53ac34a16089bf034b14e86c7
soopercool101 <soopercoolstages@gmail.com>
2020-04-10 11:28:03 -0700

    REL Stage ID now uses proper dropdown

--- BrawlLib/SSBB/ResourceNodes/Modules/RELNode.cs
+++ BrawlLib/SSBB/ResourceNodes/Modules/RELNode.cs
@@ -146,7 +146,7 @@ namespace BrawlLib.SSBB.ResourceNodes
         #region Stage module conversion - designer properties
 
         [Category("Brawl Stage Module")]
-        [TypeConverter(typeof(DropDownListStageIDs))]
+        [TypeConverter(typeof(DropDownListStageRelIDs))]
         public int? StageID
         {
             get => _stageID;
