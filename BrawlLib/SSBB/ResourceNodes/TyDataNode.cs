568bd6a8c840cb21980b790b7552652e50a827ef
soopercool101 <soopercoolstages@gmail.com>
2020-04-23 09:23:01 -0700

    Very basic TyData parsing (no saving yet)

---
+++ BrawlLib/SSBB/ResourceNodes/TyDataNode.cs
@@ -0,0 +1,236 @@
+﻿using BrawlLib.Internal;
+using BrawlLib.SSBB.Types;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrawlLib.SSBB.ResourceNodes
+{
+    public unsafe class TyDataNode : ResourceNode
+    {
+        internal TyDataHeader* Header => (TyDataHeader*)WorkingUncompressed.Address;
+
+        public override bool OnInitialize()
+        {
+            if (_name == null)
+            {
+                _name = FileName;
+            }
+            return true;
+        }
+
+        public override void OnPopulate()
+        {
+            for (int i = 0; i < Header->_entries; i++)
+            {
+                int length;
+                if (i < Header->_entries - 1)
+                {
+                    length = (int) ((*Header)[i + 1].UInt - (*Header)[i].UInt);
+                }
+                else
+                {
+                    length = (int) (Header->_dataOffset - (*Header)[i].UInt);
+                }
+
+                string name = Header->GetEntryName(i);
+
+                ResourceNode node;
+
+                switch (name)
+                {
+                    case "tySealList":
+                        node = new TySealList();
+                        break;
+                    default:
+                        node = new RawDataNode();
+                        break;
+                }
+                node._name = name;
+                node.Initialize(this, Header->GetEntry(i), length);
+            }
+        }
+
+        internal static ResourceNode TryParseGeneric(DataSource source, ResourceNode parent)
+        {
+            VoidPtr addr = source.Address;
+            TyDataHeader* header = (TyDataHeader*)addr;
+
+            if (header->_size != source.Length || header->_pad1 != 0 || header->_pad2 != 0 || header->_pad3 != 0 || header->_pad4 != 0 || header->_dataOffset > source.Length || header->_dataOffset + (header->_dataEntries * 4) > source.Length)
+            {
+                return null;
+            }
+
+            return new TyDataNode();
+        }
+    }
+
+    public unsafe class TySealList : ResourceNode
+    {
+        public override bool OnInitialize()
+        {
+            return WorkingUncompressed.Length >= 100;
+        }
+
+        public override void OnPopulate()
+        {
+            TyDataHeader* parentData = (TyDataHeader*)Parent.WorkingUncompressed.Address;
+            uint offset = 0;
+            while (offset + 100 <= WorkingUncompressed.Length)
+            {
+                //StickerEntry* e = (StickerEntry*) parentData->GetStickerEntry(Children.Count);
+                new TySealNode().Initialize(this, WorkingUncompressed.Address + offset, 100);
+                offset += 100;
+            }
+        }
+    }
+
+    public class StickerData
+    {
+        public string InternalName;
+        public string BrresName;
+    }
+
+    public unsafe class TySealNode : ResourceNode
+    {
+        internal TySeal* Header => (TySeal*)WorkingUncompressed.Address;
+
+        internal string _brres;
+
+        [Category("Sticker Data")]
+        public string BRRES
+        {
+            get => _brres;
+            set
+            {
+                _brres = value;
+                SignalPropertyChange();
+            }
+        }
+
+
+        internal int _id;
+
+        [Category("Sticker Data")]
+        public int ID
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public int _unknown0x0C;
+
+        [Category("Sticker Data")]
+        public int Unknown0x0C
+        {
+            get => _unknown0x0C;
+            set
+            {
+                _unknown0x0C = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public int _unknown0x10;
+
+        [Category("Sticker Data")]
+        public int Unknown0x10
+        {
+            get => _unknown0x10;
+            set
+            {
+                _unknown0x10 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public int _unknown0x14;
+
+
+        [Category("Sticker Data")]
+        public int Unknown0x14
+        {
+            get => _unknown0x14;
+            set
+            {
+                _unknown0x14 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public short _order1;
+
+        [Category("Sticker Data")]
+        public short Order1
+        {
+            get => _order1;
+            set
+            {
+                _order1 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public short _order2;
+
+        [Category("Sticker Data")]
+        public short Order2
+        {
+            get => _order2;
+            set
+            {
+                _order2 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public short _unknown0x20;
+
+        [Category("Sticker Data")]
+        public short Unknown0x20
+        {
+            get => _unknown0x20;
+            set
+            {
+                _unknown0x20 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public long _characterFlags;
+
+        [Category("Sticker Data")]
+        public long CharacterFlags
+        {
+            get => _characterFlags;
+            set
+            {
+                _characterFlags = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public override bool OnInitialize()
+        {
+            VoidPtr tydata = Parent.Parent.WorkingUncompressed.Address;
+            _id = Header->_id;
+            _name = (tydata + TyDataHeader.HeaderSize).GetUTF8String(Header->_nameOffset);
+            _brres = (tydata + TyDataHeader.HeaderSize).GetUTF8String(Header->_brresOffset);
+            _unknown0x0C = Header->_unknown0x0C;
+            _unknown0x10 = Header->_unknown0x10;
+            _unknown0x14 = Header->_unknown0x14;
+            _order1 = Header->_order1;
+            _order2 = Header->_order2;
+            _unknown0x20 = Header->_unknown0x20;
+            _characterFlags = Header->_characterFlags;
+            return false;
+        }
+    }
+}
---
+++ BrawlLib/SSBB/Types/TyData.cs
@@ -0,0 +1,82 @@
+﻿using BrawlLib.Internal;
+using BrawlLib.SSBB.Types.ProjectPlus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrawlLib.SSBB.Types
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct TyDataHeader
+    {
+        public static readonly uint HeaderSize = 0x20;
+        public buint _size;
+        public buint _dataOffset;
+        public buint _dataEntries;
+        public buint _entries;
+        public buint _pad1;
+        public buint _pad2;
+        public buint _pad3;
+        public buint _pad4;
+
+
+        public VoidPtr GetDataEntryOffset(int index)
+        {
+            if (index > _dataEntries)
+            {
+                return null;
+            }
+
+            return Address + HeaderSize + _dataOffset + index * 4;
+        }
+
+        public VoidPtr GetEntry(int index)
+        {
+            return Address + HeaderSize + this[index].UInt;
+        }
+
+        public string GetEntryName(int index)
+        {
+            return (Address + HeaderSize + _dataOffset + _dataEntries * 4 + _entries * TyEntry.Size +
+                   ((TyEntry*) this[index])->_strOffset).GetUTF8String();
+        }
+
+        public VoidPtr this[int index] => (byte*)Address + HeaderSize + _dataOffset + _dataEntries * 4 + index * TyEntry.Size;
+        private VoidPtr Address
+        {
+            get
+            {
+                fixed (void* ptr = &this)
+                {
+                    return ptr;
+                }
+            }
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct TyEntry
+    {
+        public static readonly uint Size = 0x08;
+        public buint _unknown;
+        public buint _strOffset;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct TySeal
+    {
+        public bint _id;
+        public bint _nameOffset;
+        public bint _brresOffset;
+        public bint _unknown0x0C;
+        public bint _unknown0x10;
+        public bint _unknown0x14;
+        public bshort _order1; // 0x1C
+        public bshort _order2; // 0x1E
+        public bshort _unknown0x20; // 0x20
+        public blong _characterFlags; //0x22
+    }
+}
