5d1797cf5efb57a17c23935f62844b2a3cb4435d
soopercool101 <soopercoolstages@gmail.com>
2020-12-09 12:23:52 -0800

    TBLV Entry now saves as the proper size

--- BrawlLib/SSBB/Types/Stage Tables/TBLVEntry.cs
+++ BrawlLib/SSBB/Types/Stage Tables/TBLVEntry.cs
@@ -6,7 +6,7 @@ namespace BrawlLib.SSBB.Types.Stage_Tables
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct TBLVEntry // Table (Lava)
     {
-        public const int Size = 0x10;
+        public const int Size = 0xC;
 
         public bfloat _height; // Lava Height
         public bfloat _unk0x4;
