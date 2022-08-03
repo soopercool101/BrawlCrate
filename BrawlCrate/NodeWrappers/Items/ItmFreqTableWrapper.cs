8961329b299cd1feda822f9c02c8ee19c335bbf4
soopercool101 <soopercoolstages@gmail.com>
2022-08-02 20:44:14 -0700

    Remove "Duplicate" action from ItmFreqTable
    There should always be a constant amount of tables

--- BrawlCrate/NodeWrappers/Items/ItmFreqTableWrapper.cs
+++ BrawlCrate/NodeWrappers/Items/ItmFreqTableWrapper.cs
@@ -9,9 +9,6 @@ namespace BrawlCrate.NodeWrappers
     {
         private static readonly ContextMenuStrip _menu;
 
-        private static readonly ToolStripMenuItem DuplicateToolStripMenuItem =
-            new ToolStripMenuItem("&Duplicate", null, DuplicateAction, Keys.Control | Keys.D);
-
         private static readonly ToolStripMenuItem ReplaceToolStripMenuItem =
             new ToolStripMenuItem("&Replace", null, ReplaceAction, Keys.Control | Keys.R);
 
@@ -21,7 +18,6 @@ namespace BrawlCrate.NodeWrappers
             _menu.Items.Add(new ToolStripMenuItem("&New Group", null, NewEntryAction, Keys.Control | Keys.H));
             _menu.Items.Add(new ToolStripSeparator());
             _menu.Items.Add(new ToolStripMenuItem("&Export", null, ExportAction, Keys.Control | Keys.E));
-            _menu.Items.Add(DuplicateToolStripMenuItem);
             _menu.Items.Add(ReplaceToolStripMenuItem);
         }
 
