c48d211f45a2e1b3aaa683cf501b70c4f7e2c17f
soopercool101 <soopercoolstages@gmail.com>
2022-11-23 11:09:50 -0700

    Add incredibly basic EPSP parsing based on existing code

---
+++ BrawlLib/SSBB/ResourceNodes/Subspace/EPSPNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrawlLib.SSBB.ResourceNodes.Subspace
+{
+    public class EPSPNode : BLOCEntryNode
+    {
+        protected override Type SubEntryType => typeof(TyDataNode);
+        internal static ResourceNode TryParse(DataSource source, ResourceNode parent)
+        {
+            return source.Tag == "EPSP" ? new EPSPNode() : null;
+        }
+    }
+}
