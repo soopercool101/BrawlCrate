302a285aa129c7621265db02a77dae7352ae1482
soopercool101 <soopercoolstages@gmail.com>
2023-02-05 12:27:16 -0700

    Rebuild outdated unedited fighter lists

--- BrawlLib/CustomLists/CustomLists.cs
+++ BrawlLib/CustomLists/CustomLists.cs
@@ -139,8 +139,10 @@ namespace BrawlLib.CustomLists
             }
         }
 
-        private static readonly byte[] listV1Hash =
-            {0xD2, 0x03, 0xA9, 0x9E, 0x92, 0xD4, 0xCC, 0xCB, 0xD4, 0xBD, 0x85, 0x28, 0xD8, 0x7C, 0x72, 0xB4};
+        private static readonly byte[][] outDatedHashes = {
+            new byte[]{ 0xD2, 0x03, 0xA9, 0x9E, 0x92, 0xD4, 0xCC, 0xCB, 0xD4, 0xBD, 0x85, 0x28, 0xD8, 0x7C, 0x72, 0xB4 },
+            new byte[]{ 0xC3, 0x85, 0x7E, 0xAF, 0x8A, 0xA0, 0xFD, 0x59, 0x8B, 0x12, 0xD9, 0xCF, 0x06, 0x7D, 0x80, 0x23 }
+        };
 
         public static bool GenerateLists()
         {
@@ -154,15 +156,18 @@ namespace BrawlLib.CustomLists
                     using (FileStream stream = File.OpenRead(listName))
                     {
                         byte[] hash = md5.ComputeHash(stream);
-                        if (hash.Length == listV1Hash.Length)
+                        foreach(var oldHash in outDatedHashes)
                         {
-                            fileOutdated = true;
-                            for (int i = 0; i < hash.Length; i++)
+                            if (hash.Length == oldHash.Length)
                             {
-                                if (hash[i] != listV1Hash[i])
+                                fileOutdated = true;
+                                for (int i = 0; i < hash.Length; i++)
                                 {
-                                    fileOutdated = false;
-                                    break;
+                                    if (hash[i] != oldHash[i])
+                                    {
+                                        fileOutdated = false;
+                                        break;
+                                    }
                                 }
                             }
                         }
