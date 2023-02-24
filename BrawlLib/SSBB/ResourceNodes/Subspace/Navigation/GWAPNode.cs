2675d1a810860cf3bafbf405d9fcfac395fe26ca
soopercool101 <soopercoolstages@gmail.com>
2023-02-23 17:27:17 -0700

    Add editing support for GWAP Warp Zones

---
+++ BrawlLib/SSBB/ResourceNodes/Subspace/Navigation/GWAPNode.cs
@@ -0,0 +1,332 @@
+﻿using BrawlLib.Internal;
+using BrawlLib.SSBB.Types.Subspace.Navigation;
+using System;
+using System.ComponentModel;
+
+namespace BrawlLib.SSBB.ResourceNodes.Subspace.Navigation
+{
+    public class GWAPNode : BLOCEntryNode
+    {
+        public override Type SubEntryType => typeof(GWAPEntryNode);
+        protected override string baseName => "Warp Zone";
+
+        internal static ResourceNode TryParse(DataSource source, ResourceNode parent)
+        {
+            return source.Tag == "GWAP" ? new GWAPNode() : null;
+        }
+    }
+
+    public unsafe class GWAPEntryNode : ResourceNode
+    {
+        public GWAPEntry Data;
+
+        public MotionPathDataClass _motionPathData;
+        [Category("Warp Zone")]
+        [TypeConverter(typeof(ExpandableObjectCustomConverter))]
+        public MotionPathDataClass MotionPathData
+        {
+            get => _motionPathData;
+            set
+            {
+                _motionPathData = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x08
+        {
+            get => Data._unknown0x08;
+            set
+            {
+                Data._unknown0x08 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x0C
+        {
+            get => Data._unknown0x0C;
+            set
+            {
+                Data._unknown0x0C = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x10
+        {
+            get => Data._unknown0x10;
+            set
+            {
+                Data._unknown0x10 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x14
+        {
+            get => Data._unknown0x14;
+            set
+            {
+                Data._unknown0x14 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x18
+        {
+            get => Data._unknown0x18;
+            set
+            {
+                Data._unknown0x18 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x1C
+        {
+            get => Data._unknown0x1C;
+            set
+            {
+                Data._unknown0x1C = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Warp Zone")]
+        [TypeConverter(typeof(Vector2StringConverter))]
+        public Vector2 AreaOffset
+        {
+            get => new Vector2(Data._areaOffsetPositionX, Data._areaOffsetPositionY);
+            set
+            {
+                Data._areaOffsetPositionX = value.X;
+                Data._areaOffsetPositionY = value.Y;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Warp Zone")]
+        [TypeConverter(typeof(Vector2StringConverter))]
+        public Vector2 AreaRange
+        {
+            get => new Vector2(Data._areaRangeX, Data._areaRangeY);
+            set
+            {
+                Data._areaRangeX = value.X;
+                Data._areaRangeY = value.Y;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Warp Zone")]
+        public string BoneName
+        {
+            get => Data.BoneName;
+            set
+            {
+                Data.BoneName = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Warp Zone")]
+        public byte ModelDataIndex
+        {
+            get => Data._modelDataIndex;
+            set
+            {
+                Data._modelDataIndex = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public byte Unknown0x51
+        {
+            get => Data._unknown0x51;
+            set
+            {
+                Data._unknown0x51 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public byte Unknown0x52
+        {
+            get => Data._unknown0x52;
+            set
+            {
+                Data._unknown0x52 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public byte Unknown0x53
+        {
+            get => Data._unknown0x53;
+            set
+            {
+                Data._unknown0x53 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Warp Zone")]
+        [TypeConverter(typeof(Vector2StringConverter))]
+        public Vector2 Position
+        {
+            get => new Vector2(Data._positionX, Data._positionY);
+            set
+            {
+                Data._positionX = value.X;
+                Data._positionY = value.Y;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Warp Zone")]
+        [TypeConverter(typeof(HexUIntConverter))]
+        public uint SoundId1
+        {
+            get => Data._soundId1;
+            set
+            {
+                Data._soundId1 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Warp Zone")]
+        [TypeConverter(typeof(HexUIntConverter))]
+        public uint SoundId2
+        {
+            get => Data._soundId2;
+            set
+            {
+                Data._soundId2 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x64
+        {
+            get => Data._unknown0x64;
+            set
+            {
+                Data._unknown0x64 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x68
+        {
+            get => Data._unknown0x68;
+            set
+            {
+                Data._unknown0x68 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public TriggerDataClass _warpTriggerData;
+        [Category("Warp Zone")]
+        [TypeConverter(typeof(ExpandableObjectCustomConverter))]
+        public TriggerDataClass WarpTriggerData
+        {
+            get => _warpTriggerData;
+            set
+            {
+                _warpTriggerData = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x70
+        {
+            get => Data._unknown0x70;
+            set
+            {
+                Data._unknown0x70 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x74
+        {
+            get => Data._unknown0x74;
+            set
+            {
+                Data._unknown0x74 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public uint Unknown0x78
+        {
+            get => Data._unknown0x78;
+            set
+            {
+                Data._unknown0x78 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public TriggerDataClass _isValidTriggerData;
+        [Category("Warp Zone")]
+        [TypeConverter(typeof(ExpandableObjectCustomConverter))]
+        public TriggerDataClass IsValidTriggerData
+        {
+            get => _isValidTriggerData;
+            set
+            {
+                _isValidTriggerData = value;
+                SignalPropertyChange();
+            }
+        }
+
+        public GWAPEntryNode()
+        {
+            _motionPathData = new MotionPathDataClass(this);
+            _warpTriggerData = new TriggerDataClass(this);
+            _isValidTriggerData = new TriggerDataClass(this);
+        }
+
+        public override bool OnInitialize()
+        {
+            Data = *(GWAPEntry*)WorkingUncompressed.Address;
+            _motionPathData = new MotionPathDataClass(this, Data._motionPathData);
+            _warpTriggerData = new TriggerDataClass(this, Data._warpTriggerData);
+            _isValidTriggerData = new TriggerDataClass(this, Data._isValidTriggerData);
+
+            return false;
+        }
+
+        public override int OnCalculateSize(bool force)
+        {
+            return GWAPEntry.Size;
+        }
+
+        public override void OnRebuild(VoidPtr address, int length, bool force)
+        {
+            GWAPEntry* hdr = (GWAPEntry*)address;
+            Data._motionPathData = _motionPathData;
+            Data._warpTriggerData = _warpTriggerData;
+            Data._isValidTriggerData = _isValidTriggerData;
+            *hdr = Data;
+        }
+    }
+}
---
+++ BrawlLib/SSBB/Types/Subspace/Navigation/GWAP.cs
@@ -0,0 +1,57 @@
+﻿using BrawlLib.Internal;
+using BrawlLib.SSBB.Types.Subspace.Triggers;
+using System.Runtime.InteropServices;
+
+namespace BrawlLib.SSBB.Types.Subspace.Navigation
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct GWAPEntry
+    {
+        public const int Size = 0x80;
+
+        public MotionPathData _motionPathData;  // 0x00
+        public buint _unknown0x08;
+        public buint _unknown0x0C;
+        public buint _unknown0x10;
+        public buint _unknown0x14;
+        public buint _unknown0x18;
+        public buint _unknown0x1C;
+        public bfloat _areaOffsetPositionX;     // 0x20
+        public bfloat _areaOffsetPositionY;     // 0x24
+        public bfloat _areaRangeX;              // 0x28
+        public bfloat _areaRangeY;              // 0x2C
+        public fixed byte _boneName[0x20];      // 0x30
+        public byte _modelDataIndex;            // 0x50
+        public byte _unknown0x51;
+        public byte _unknown0x52;
+        public byte _unknown0x53;
+        public bfloat _positionX;               // 0x54
+        public bfloat _positionY;               // 0x58
+        public buint _soundId1;                 // 0x5C
+        public buint _soundId2;                 // 0x60
+        public buint _unknown0x64;
+        public buint _unknown0x68;
+        public TriggerData _warpTriggerData;    // 0x6C
+        public buint _unknown0x70;
+        public buint _unknown0x74;
+        public buint _unknown0x78;
+        public TriggerData _isValidTriggerData; // 0x7C
+
+        public string BoneName
+        {
+            get => Address.GetUTF8String(0x30);
+            set => Address.WriteUTF8String(value, 0x30, 0x20);
+        }
+
+        private VoidPtr Address
+        {
+            get
+            {
+                fixed (void* ptr = &this)
+                {
+                    return ptr;
+                }
+            }
+        }
+    }
+}
