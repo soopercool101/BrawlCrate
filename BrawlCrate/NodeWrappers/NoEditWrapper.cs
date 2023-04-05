cd12d3a286d09487e468d0c1e2d8866e5856e11e
soopercool101 <soopercoolstages@gmail.com>
2023-04-04 23:14:06 -0700

    Enable replace for ItmParam

--- BrawlCrate/NodeWrappers/NoEditWrapper.cs
+++ BrawlCrate/NodeWrappers/NoEditWrapper.cs
@@ -5,7 +5,6 @@ using System.Windows.Forms;
 namespace BrawlCrate.NodeWrappers
 {
     [NodeWrapper(ResourceType.NoEditEntry)]
-    [NodeWrapper(ResourceType.NoEditItem)]
     public class NoEditEntryWrapper : GenericWrapper
     {
         private static readonly ContextMenuStrip _menu;
@@ -60,6 +59,66 @@ namespace BrawlCrate.NodeWrappers
         }
 
         public NoEditEntryWrapper()
+        {
+            ContextMenuStrip = _menu;
+        }
+    }
+
+    [NodeWrapper(ResourceType.NoEditItem)]
+    public class NoEditItemWrapper : GenericWrapper
+    {
+        private static readonly ContextMenuStrip _menu;
+
+        private static readonly ToolStripMenuItem DuplicateToolStripMenuItem =
+            new ToolStripMenuItem("&Duplicate", null, DuplicateAction, Keys.Control | Keys.D);
+
+        private static readonly ToolStripMenuItem ReplaceToolStripMenuItem =
+            new ToolStripMenuItem("&Replace", null, ReplaceAction, Keys.Control | Keys.R);
+
+        private static readonly ToolStripMenuItem RestoreToolStripMenuItem =
+            new ToolStripMenuItem("Res&tore", null, RestoreAction, Keys.Control | Keys.T);
+
+        private static readonly ToolStripMenuItem MoveUpToolStripMenuItem =
+            new ToolStripMenuItem("Move &Up", null, MoveUpAction, Keys.Control | Keys.Up);
+
+        private static readonly ToolStripMenuItem MoveDownToolStripMenuItem =
+            new ToolStripMenuItem("Move D&own", null, MoveDownAction, Keys.Control | Keys.Down);
+
+        private static readonly ToolStripMenuItem DeleteToolStripMenuItem =
+            new ToolStripMenuItem("&Delete", null, DeleteAction, Keys.Control | Keys.Delete);
+
+        static NoEditItemWrapper()
+        {
+            _menu = new ContextMenuStrip();
+            _menu.Items.Add(new ToolStripMenuItem("&Export", null, ExportAction, Keys.Control | Keys.E));
+            _menu.Items.Add(ReplaceToolStripMenuItem);
+            _menu.Opening += MenuOpening;
+            _menu.Closing += MenuClosing;
+        }
+
+        private static void MenuClosing(object sender, ToolStripDropDownClosingEventArgs e)
+        {
+            DuplicateToolStripMenuItem.Enabled = true;
+            ReplaceToolStripMenuItem.Enabled = true;
+            RestoreToolStripMenuItem.Enabled = true;
+            MoveUpToolStripMenuItem.Enabled = true;
+            MoveDownToolStripMenuItem.Enabled = true;
+            DeleteToolStripMenuItem.Enabled = true;
+        }
+
+        private static void MenuOpening(object sender, CancelEventArgs e)
+        {
+            NoEditItemWrapper w = GetInstance<NoEditItemWrapper>();
+
+            DuplicateToolStripMenuItem.Enabled = w.Parent != null;
+            ReplaceToolStripMenuItem.Enabled = w.Parent != null;
+            RestoreToolStripMenuItem.Enabled = w._resource.IsDirty || w._resource.IsBranch;
+            MoveUpToolStripMenuItem.Enabled = w.PrevNode != null;
+            MoveDownToolStripMenuItem.Enabled = w.NextNode != null;
+            DeleteToolStripMenuItem.Enabled = w.Parent != null;
+        }
+
+        public NoEditItemWrapper()
         {
             ContextMenuStrip = _menu;
         }
