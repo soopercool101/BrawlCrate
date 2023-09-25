8c5118fb089f7d9f29d987e652de104e06c1994e
soopercool101 <soopercoolstages@gmail.com>
2023-09-25 14:35:39 -0700

    Add support for GFIN Final Destination Gimmick files

---
+++ BrawlLib/SSBB/ResourceNodes/Subspace/Other/GFINNode.cs
@@ -0,0 +1,87 @@
+﻿using BrawlLib.Internal;
+using BrawlLib.SSBB.Types.Subspace.Hazards;
+using System;
+using System.ComponentModel;
+
+namespace BrawlLib.SSBB.ResourceNodes
+{
+    public class GFINNode : BLOCEntryNode
+    {
+        public override Type SubEntryType => typeof(GFINEntryNode);
+        protected override string baseName => "Final Destination Gimmicks";
+
+        internal static ResourceNode TryParse(DataSource source, ResourceNode parent)
+        {
+            return source.Tag == "GFIN" ? new GFINNode() : null;
+        }
+    }
+
+    public unsafe class GFINEntryNode : ResourceNode
+    {
+        protected internal GFINEntry Data;
+
+        [Category("GFIN")]
+        public byte BackgroundModelData
+        {
+            get => Data._backgroundModelData;
+            set
+            {
+                Data._backgroundModelData = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public byte Unknown0x01
+        {
+            get => Data._unknown0x01;
+            set
+            {
+                Data._unknown0x01 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public byte Unknown0x02
+        {
+            get => Data._unknown0x02;
+            set
+            {
+                Data._unknown0x02 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        [Category("Unknown")]
+        public byte Unknown0x03
+        {
+            get => Data._unknown0x03;
+            set
+            {
+                Data._unknown0x03 = value;
+                SignalPropertyChange();
+            }
+        }
+
+        private TriggerDataClass _trigger;
+
+        public override bool OnInitialize()
+        {
+            Data = *(GFINEntry*)WorkingUncompressed.Address;
+
+            return base.OnInitialize();
+        }
+
+        public override int OnCalculateSize(bool force)
+        {
+            return GFINEntry.Size;
+        }
+
+        public override void OnRebuild(VoidPtr address, int length, bool force)
+        {
+            GFINEntry* header = (GFINEntry*)address;
+            *header = Data;
+        }
+    }
+}
---
+++ BrawlLib/SSBB/Types/Subspace/Other/GFIN.cs
@@ -0,0 +1,27 @@
+﻿using BrawlLib.Internal;
+using System.Runtime.InteropServices;
+
+namespace BrawlLib.SSBB.Types.Subspace.Hazards
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct GFINEntry
+    {
+        public const int Size = 0x04;
+
+        public byte _backgroundModelData;
+        public byte _unknown0x01;
+        public byte _unknown0x02;
+        public byte _unknown0x03;
+
+        private VoidPtr Address
+        {
+            get
+            {
+                fixed (void* ptr = &this)
+                {
+                    return ptr;
+                }
+            }
+        }
+    }
+}
