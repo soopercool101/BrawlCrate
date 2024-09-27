29e6b0538fd10a5c172ccd881265082e8d5e64b9
squidgy617 <ryantheroux@yahoo.com>
2024-09-26 21:19:06 -0500

    Use Application.DoEvents instead of Sleep to wait for population to complete

--- BrawlLib/SSBB/ResourceNodes/Modules/RELNode.cs
+++ BrawlLib/SSBB/ResourceNodes/Modules/RELNode.cs
@@ -298,25 +298,21 @@ namespace BrawlLib.SSBB.ResourceNodes
 
         public override void Dispose()
         {
+            // If not populated, try to give it time to finish
             if (!Populated)
             {
-                int timeoutSecs = 5;
-                // wait 5 seconds to see if population finishes
-                while (!Populated && timeoutSecs >= 0)
+                var sw = new Stopwatch();
+                sw.Start();
+                while (!Populated)
                 {
-                    Thread.Sleep(1000);
-                    --timeoutSecs;
-                }
-                // attempt to cancel population if it hasn't finished still
-                if (!Populated)
-                {
-                    populator.CancelAsync();
-                    for (int i = 0; i < 5; i++) // wait 5 more seconds for cancellation to finish hopefully
+                    Application.DoEvents();
+                    // Wait max 5 seconds before forcefully disposing
+                    if (sw.ElapsedMilliseconds > 5000)
                     {
-                        Thread.Sleep(1000);
+                        break;
                     }
-                    Debug.WriteLine("Cancelled population");
                 }
+                sw.Stop();
             }
             populator?.Dispose();
             _files.Remove(ModuleID);
